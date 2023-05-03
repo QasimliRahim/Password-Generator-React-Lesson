e12a8b8eff2c9a13c0135feac100ab1eb653ca89
Huseyn Mirzayev <70962188+hsynmrzyv@users.noreply.github.com>
2023-05-03 13:23:16 +0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import "./App.css";
+
+// Data
+import { upperCaseLetters, lowerCaseLetters, numbers, special } from "./data";
+
+// Components
+import Modal from "./Components/Modal";
+
+function App() {
+  return (
+    <div className="App">
+      {/* Modal */}
+      <div className="backdrop"></div>
+      <div className="modal">
+        <h2>Success</h2>
+        <p>Password successfully copied to clipboard</p>
+        <button>Okay</button>
+      </div>
+      {/* Modal */}
+      <div className="generator">
+        <form className="generator__form">
+          <div className="generator__form-controls">
+            <div className="generator__form-control">
+              <label htmlFor="uppercase">Uppercase</label>
+              <input type="checkbox" id="uppercase" name="uppercase" />
+            </div>
+            <div className="generator__form-control">
+              <label htmlFor="lowercase">Lowercase</label>
+              <input type="checkbox" id="lowercase" name="lowercase" />
+            </div>
+            <div className="generator__form-control">
+              <label htmlFor="numbers">Numbers</label>
+              <input type="checkbox" id="numbers" name="numbers" />
+            </div>
+            <div className="generator__form-control">
+              <label htmlFor="symbols">Symbols</label>
+              <input type="checkbox" id="symbols" name="symbols" />
+            </div>
+            <div className="generator__length">
+              <h4 className="generator__length-title">Password Length</h4>
+              <div className="generator__length-counter">
+                <button>-</button>
+                <span>0</span>
+                <button>+</button>
+              </div>
+            </div>
+
+            <div className="generator__form-actions">
+              <button className="btn generate-btn">Generate Password</button>
+              <button className="btn copy-btn">Copy Password</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
